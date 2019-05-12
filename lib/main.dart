e8536ddde53b84ee8a8539591fb73115b20180b3
Vignesh M <vigneshm@agiratech.com>
2019-05-12 21:16:38 +0530

    Finished implementing the challenge app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: BallPage(),
+          backgroundColor: Colors.blue,
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int num = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$num.png'),
+        onPressed: () {
+          setState(() {
+            this.num = Random().nextInt(4) + 1;
+          });
+        },
+      ),
+    );
+  }
+}
